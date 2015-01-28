34a3bf9fb3a587302e2bb96105c385fd86e613f0
andrew-kirkham <andrew.kirkham@marquette.edu>
2015-01-27 21:54:18 -0600

    added project/workspace files removed board class framework for main

--- tic_tac_toe/src/main.c
+++ tic_tac_toe/src/main.c
@@ -1,8 +1,32 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
 
+char board[3][3] = {{ 0, 1, 2 }, { 3, 4, 5 }, { 6, 7, 8 }};
+
+void reset();
+void logWinner();
+void playGame(int);
+
+
+void simulate(int iterations){
+	
+	for (int i=0; i< iterations; i++){
+		int startingPlayer = rand() % 2;
+		playGame(startingPlayer);
+		logWinner();
+		reset();
+	}
+}
 
 int main(int argc, char **argv)
 {
-	printf("hello world\n");
-	return 0;
+	int iterations  = 0;
+	std::cout << "Please enter the number of games to simulate: \n"
+	cin >> iterations ;
+	
+	simulate(iterations);
+	
+	cout << "Simulation Complete";
+	cout << "View 'output.txt' to view results";
 }
